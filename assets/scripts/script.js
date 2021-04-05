babd3816fa08991f60f35750c5880a267463e9f9
Bryan Barnes <bryan.barnes12@gmail.com>
2021-04-05 12:24:13 -0500

    added more functionality to .js(flow functions and global variables) and some basic styling.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,48 +1,108 @@
 let highScores = [["init", "bmb", "score", "0"]];
 let currentScore = [];
+let welcomeText = "Welcome to the coding game. This is the game. This is how to play. Click the button below to begin.";
+var time = 60;
+
+const questions = [{question: "question 1", "answer1": "answer 1", "answer2": "answer 2", "answer3": "answer 3", "answer4": "answer 4", "correctAns": "answer3"}, {"question": "question 2", "answer1": "answer 1", "answer2": "answer 2", "answer3": "answer 3", "answer4": "answer 4", "correctAns": "answer3"}, {"question": "question 3", "answer1": "answer 1", "answer2": "answer 2", "answer3": "answer 3", "answer4": "answer 4", "correctAns": "answer3"}]
+let questionNum = 0;
+console.log(questions[questionNum]);
 function generateHeader() {
-    var scoreDiv = document.createElement("DIV");
-    var headlineDiv = document.createElement("DIV");
-    var timerDiv = document.createElement("DIV");
+    let scoreDiv = document.createElement("DIV");
+    let headlineDiv = document.createElement("DIV");
+    let timerDiv = document.createElement("DIV");
     scoreDiv.innerHTML = "TEST";
     scoreDiv.id = "score"
     headlineDiv.innerHTML = "Coding Quiz";
     headlineDiv.id = "headline";
-    timerDiv.innerHTML = "TEST";
+    timerDiv.innerHTML = "1:00";
     timerDiv.id = "timer";
-    var test = document.getElementById("header");
-    test.appendChild(scoreDiv);
-    test.appendChild(headlineDiv);
-    test.appendChild(timerDiv);
-    console.log(test);
+    let header = document.getElementById("header");
+    header.appendChild(scoreDiv);
+    header.appendChild(headlineDiv);
+    header.appendChild(timerDiv);
+    console.log(header);
 }
-function generateMain() {
-    var btn1 = document.createElement("DIV");
-    btn1.innerHTML = "TEST";
-    var test = document.getElementById("main");
-    test.appendChild(btn1);
+function generateWelcome() {
+    let main = document.getElementById("main");
+    if (questionNum !== 0) {
+        main.removeChild(main.childNodes[1]);
+    }
+    let newDiv = document.createElement("DIV");
+    newDiv.id = "welcome";
+    main.appendChild(newDiv);
+    let welcome = document.getElementById("welcome");
+    let newH3 = document.createElement("H3");
+    newH3.innerHTML = "javaScript Coding Game"
+    let newP = document.createElement("P");
+    newP.innerHTML = welcomeText;
+    let newButton = document.createElement("BUTTON");
+    newButton.id = "startButton";
+    newButton.innerHTML = "begin game";
+    welcome.appendChild(newH3);
+    welcome.appendChild(newP);
+    welcome.appendChild(newButton);
+    console.log(main);
+    document.getElementById("startButton").addEventListener("click", function () {
+        generateQuestion(questionNum);
+    });
+
 }
+
+function generateQuestion(questionNum) {
+    if (time === 60) {
+        countDown();
+    }
+    let main = document.getElementById("main");
+    main.removeChild(main.childNodes[1]);
+    let questionDiv = document.createElement("DIV");
+    questionDiv.id = "questionDiv";
+    main.appendChild(questionDiv);
+    questionDiv = document.getElementById("questionDiv");
+
+    let newH3 = document.createElement("H3");
+    newH3.innerHTML = questions[questionNum].question;
+    // let newP = document.createElement("P");
+    // newP.innerHTML = welcomeText;
+    // let newButton = document.createElement("BUTTON");
+    // newButton.id = "startButton";
+    // newButton.innerHTML = "begin game";
+    questionDiv.appendChild(newH3);
+    // welcome.appendChild(newP);
+    // welcome.appendChild(newButton);
+    console.log(main);
+    // document.getElementById("startButton").addEventListener("click", generateQuestion);
+
+
+
+
+    questionNum++;
+}
+
 function replaceMain() {
-    var btn1 = document.createElement("DIV");
-    btn1.innerHTML = "different test";
-    var test = document.getElementById("main");
-    test.appendChild(btn1);
+    let mainDiv = document.getElementById("main");
+    mainDiv.innerHTML = "different (for sure different) test";
 }
 function countDown() {
     var countSpan = document.getElementById("timer");
-    var time = 60;
     var id = setInterval(reduceTime, 1000);
     function reduceTime() {
       if (time == 0) {
         clearInterval(id);
       } else {
-        time--; 
-        countSpan.innerText = time; 
+          if (time > 10) {
+            time--; 
+            countSpan.innerText = `0:${time}`; 
+          } else {
+            time--; 
+            countSpan.innerText = `0:0${time}`; 
+    
+          }
       }
     }
 }
 
-
+generateHeader();
+generateWelcome();
 
 
 
@@ -63,7 +123,15 @@ document.getElementById("test1").addEventListener("click", function () {
     generateHeader();
     countDown();
 });
-document.getElementById("test2").addEventListener("click", generateMain);
-document.getElementById("test3").addEventListener("click", replaceMain);
+// document.getElementById("startButton").addEventListener("click", function () {
+//     generateHeader();
+//     countDown();
+//     generateQuestion();
+// });
+document.getElementById("test2").addEventListener("click", generateWelcome);
+document.getElementById("test3").addEventListener("click", generateQuestion);
 document.getElementById("test4").addEventListener("click", displayScores);
-
+// document.getElementById("main").addEventListener("load", function () {
+//     generateHeader();
+//     generateWelcome();
+// });
