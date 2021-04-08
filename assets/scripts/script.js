becf2b236fb8d4236aab35e36259a45ebb1f6985
Bryan Barnes <bryan.barnes12@gmail.com>
2021-04-08 12:41:18 -0500

    Finished styling and commented the .js file.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,14 @@
+// Global variables to be used throughout the program.
 let highScores = [];
 let currentScore = 0;
 let currentScoreMult = 0;
 let score = 0;
 let userInitials = "";
-
 let time = 60;
 let clockOperator = true;
+let questionNum = 0;
+
+// Question array containing question objects for each question.
 const questions = [{
     question: "Which of the following is a JavaScript primitive data type?",
     answer1: "String",
@@ -42,54 +45,113 @@ const questions = [{
     answer4: "initialization, stopping condition, iteration statement",
     correctAns: "answer4"
 }, ""]
-let questionNum = 0;
+
+// Generates header on page load.
 function generateHeader() {
+    // Naming the variables for element creation.
     let scoreDiv = document.createElement("DIV");
     let headlineDiv = document.createElement("DIV");
     let timerDiv = document.createElement("DIV");
+    // Setting element attributes.
     scoreDiv.innerHTML = "high scores";
     scoreDiv.id = "score"
     headlineDiv.id = "headline";
     timerDiv.innerHTML = "1:00";
     timerDiv.id = "timer";
+    // Naming the container variable and creating the child elements within.
     let header = document.getElementById("header");
     header.appendChild(scoreDiv);
     header.appendChild(headlineDiv);
     header.appendChild(timerDiv);
-    console.log(header);
+    // Naming the child elements and setting their internal values.
     let headline = document.getElementById("headline");
     let quizLogo = document.createElement("h2");
     quizLogo.innerHTML = "JavaScript Coding Game";
     headline.appendChild(quizLogo);
+    // Event listener for the high scores section.
     document.getElementById("score").addEventListener("click", function (event){event.preventDefault();registerScore();displayScores();});
 
 }
-let welcomeText = "Welcome to the coding quiz. The elements of this quiz are created dynamically using javascript. To play, click the 'begin game' button and answer the questions as quickly and accurately as you can.";
+
+// Generates welcome text on page load.
 function generateWelcome() {
-    let main = document.getElementById("main");
-    if (questionNum !== 0) {
-        main.removeChild(main.childNodes[0]);
-    }
+    // Creating the container div.
     let newDiv = document.createElement("DIV");
     newDiv.id = "welcome";
     main.appendChild(newDiv);
+    // Creating the elements to be displayed in the welcome section.
     let welcome = document.getElementById("welcome");
     let newH3 = document.createElement("H3");
-    newH3.innerHTML = "JavaScript Coding Game"
+    newH3.innerHTML = "JavaScript Coding Game";
     let newP = document.createElement("P");
-    newP.innerHTML = welcomeText;
+    newP.innerHTML = "Welcome to the coding quiz. The elements of this quiz are created dynamically using javascript. To play, click the 'begin game' button and answer the questions as quickly and accurately as you can.";
     let newButton = document.createElement("BUTTON");
     newButton.id = "startButton";
     newButton.innerHTML = "begin game";
     welcome.appendChild(newH3);
     welcome.appendChild(newP);
     welcome.appendChild(newButton);
-    console.log(main);
+    // Start button event listener.
     document.getElementById("startButton").addEventListener("click", function () {
     generateQuestion(questionNum);
     });
 
 }
+
+// Generates the questions as the user moves through the quiz.
+function generateQuestion(questionNum) {
+    // Handles clock operation; starts on first question only.
+    if (time === 60 && clockOperator === true) {
+        countDown();
+    }
+    // Removing previous content in the container.
+    let main = document.getElementById("main");
+    main.removeChild(main.childNodes[1]);
+    // Creating the container for the question section.
+    let questionDiv = document.createElement("DIV");
+    questionDiv.id = "questionDiv";
+    main.appendChild(questionDiv);
+    // Creating the elements to be displayed in the question container.
+    questionDiv = document.getElementById("questionDiv");
+    let newH3 = document.createElement("H3");
+    newH3.innerHTML = questions[questionNum].question;
+    questionDiv.appendChild(newH3);
+    // Generating the answer buttons that go with the corresponding question.
+    for (let i = 0; i < 4; i++) {
+        let x = "answer" + (1 + i);
+        questionDiv.appendChild(document.createElement("button"));
+        questionDiv.children[i + 1].setAttribute("id", "answer" + (1 + i));
+        questionDiv.children[i + 1].innerHTML = questions[questionNum][x];
+    }
+    // Handling question scoring.
+    let correctAnswer = questions[questionNum].correctAns;
+    function scoreAndMoveOn(x) {
+        if (correctAnswer == x) {
+            currentScore += 5;
+        } else {
+            time -= 10;
+        }
+        if (questionNum < 5) {
+            generateQuestion(questionNum);
+        } else {
+            currentScoreMult = Math.floor(time / 6);
+            score = currentScore * currentScoreMult;
+            console.clear();
+            console.log(score);
+            generateResults();
+            clockOperator = false;
+        }
+    }
+    // Event listeners for the individual question buttons.
+    document.getElementById("answer1").addEventListener("click", function (event){event.preventDefault();scoreAndMoveOn("answer1");});
+    document.getElementById("answer2").addEventListener("click", function (event){event.preventDefault();scoreAndMoveOn("answer2");});
+    document.getElementById("answer3").addEventListener("click", function (event){event.preventDefault();scoreAndMoveOn("answer3");});
+    document.getElementById("answer4").addEventListener("click", function (event){event.preventDefault();scoreAndMoveOn("answer4");});
+    // Incrementing the question variable in order to move to the next question.
+    questionNum++;
+}
+
+// Generates the user's quiz results and high score entry form.
 function generateResults() {
     // Removing the last element to fill the main section.
     let main = document.getElementById("main");
@@ -112,12 +174,13 @@ function generateResults() {
     newInput.id = "userInitials"
     newButton.id = "registerButton";
     newButton.innerHTML = "submit score";
-    // Calling for the 
+    // Calling for the elements to be created on the page.
     results.appendChild(newH1);
     results.appendChild(newP);
     results.appendChild(newInputLabel);
     results.appendChild(newInput);
     results.appendChild(newButton);
+    // Event listener for the user initials/score form.
     document.getElementById("registerButton").addEventListener("click", function () {
         clockOperator = true;
         time = 60;
@@ -130,55 +193,44 @@ function generateResults() {
     questionNum = 0;
 }
 
-function generateQuestion(questionNum) {
-    if (time === 60 && clockOperator === true) {
-        countDown();
-    }
+// Displays the high scores stored on the local machine.
+function displayScores() {
+    scoreList = JSON.parse(localStorage.getItem("highScores"));
     let main = document.getElementById("main");
     main.removeChild(main.childNodes[1]);
-    let questionDiv = document.createElement("DIV");
-    questionDiv.id = "questionDiv";
-    main.appendChild(questionDiv);
-    questionDiv = document.getElementById("questionDiv");
-    let newH3 = document.createElement("H3");
-    newH3.innerHTML = questions[questionNum].question;
-    questionDiv.appendChild(newH3);
-    for (let i = 0; i < 4; i++) {
-        let x = "answer" + (1 + i);
-        questionDiv.appendChild(document.createElement("button"));
-        questionDiv.children[i + 1].setAttribute("id", "answer" + (1 + i));
-        questionDiv.children[i + 1].innerHTML = questions[questionNum][x];
+    // create the high score div.
+    let newDiv = document.createElement("DIV");
+    newDiv.id = "highScores";
+    main.appendChild(newDiv);
+    let scorePage = document.getElementById("highScores");
+    // create the high score ul.
+    let newUl = document.createElement("ul");
+    newUl.id = "scoreList"
+    scorePage.appendChild(newUl);
+    let scoreLi = document.getElementById("scoreList");
+    // create the list elements.
+    for (let i = 0; i < highScores.length; i++) {
+        let newLi = document.createElement("li");
+        newLi.innerHTML = `${i + 1}: ${highScores[i][1]} - ${highScores[i][0]}`;
+        scoreLi.appendChild(newLi);
     }
-    
-    let correctAnswer = questions[questionNum].correctAns;
-    function scoreAndMoveOn(x) {
-        if (correctAnswer == x) {
-            currentScore += 5;
-        } else {
-            time -= 10;
-        }
-        if (questionNum < 5) {
-            generateQuestion(questionNum);
-        } else {
-            currentScoreMult = Math.floor(time / 6);
-            score = currentScore * currentScoreMult;
-            console.clear();
-            console.log(score);
-            generateResults();
-            clockOperator = false;
-        }
-    }
-
-    console.log(questionNum);
-    document.getElementById("answer1").addEventListener("click", function (event){event.preventDefault();scoreAndMoveOn("answer1");});
-    document.getElementById("answer2").addEventListener("click", function (event){event.preventDefault();scoreAndMoveOn("answer2");});
-    document.getElementById("answer3").addEventListener("click", function (event){event.preventDefault();scoreAndMoveOn("answer3");});
-    document.getElementById("answer4").addEventListener("click", function (event){event.preventDefault();scoreAndMoveOn("answer4");});
-    questionNum++;
+    // Creating the play again button
+    let newButton = document.createElement("BUTTON");
+    newButton.id = "restartButton";
+    newButton.innerHTML = "play again";
+    scorePage.appendChild(newButton);
+    // EVENT LISTENER FOR THE DISPLAY SCORES PAGE
+    document.getElementById("restartButton").addEventListener("click", function () {
+        clockOperator = true;
+        time = 60;
+        generateQuestion(questionNum);
+        currentScore = 0;
+    });
 }
 
+// Handles the countdown clock.
 function countDown() {
-    var countSpan = document.getElementById("timer");
+    let countSpan = document.getElementById("timer");
     let id = setInterval(reduceTime, 1000);
     function reduceTime() {
       if (time == 0 || clockOperator == false) {
@@ -197,14 +249,7 @@ function countDown() {
     }
 }
 
-generateHeader();
-generateWelcome();
-
-
-
-
-
-
+// Registers and logs the scores to the local machine.
 function registerScore() {
     if (localStorage.getItem("highScores") === null) {
         highScores = [[0, "aaa"], [0, "aaa"], [0, "aaa"], [0, "aaa"], [0, "aaa"]];
@@ -230,37 +275,7 @@ function registerScore() {
     //highScores = localScores.reverse();
     localStorage.setItem("highScores", JSON.stringify(highScores));
 }
-function displayScores() {
-    scoreList = JSON.parse(localStorage.getItem("highScores"));
-    let main = document.getElementById("main");
-    main.removeChild(main.childNodes[1]);
-    // create the high score div.
-    let newDiv = document.createElement("DIV");
-    newDiv.id = "highScores";
-    main.appendChild(newDiv);
-    let scorePage = document.getElementById("highScores");
-    // create the high score ul.
-    let newUl = document.createElement("ul");
-    newUl.id = "scoreList"
-    scorePage.appendChild(newUl);
-    let scoreLi = document.getElementById("scoreList");
-    // create the list elements.
-    for (let i = 0; i < highScores.length; i++) {
-        let newLi = document.createElement("li");
-        newLi.innerHTML = `${i + 1}: ${highScores[i][1]} - ${highScores[i][0]}`;
-        scoreLi.appendChild(newLi);
-    }
-    let newButton = document.createElement("BUTTON");
-    newButton.id = "restartButton";
-    newButton.innerHTML = "play again";
-    // Calling for the 
-    scorePage.appendChild(newButton);
-    // EVENT LISTENER FOR THE DISPLAY SCORES PAGE
-    document.getElementById("restartButton").addEventListener("click", function () {
-        clockOperator = true;
-        time = 60;
-        generateQuestion(questionNum);
-        currentScore = 0;
-    });
 
-}
+// Calls for the initial page elements to be displayed.
+generateHeader();
+generateWelcome();
